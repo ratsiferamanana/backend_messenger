074a99f74a59bf0a22dcc0392197f50ac746dc28
ratsiferamanana <hei.jimmy.5@gmail.com>
2025-02-17 11:00:12 +0300

    fix: ajout de nouvelle postgres db

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
-const {neon} = require("@neondatabase/serverless");
-const sql = neon(process.env.DATABASE_URL);
-module.exports = sql;
+const {Pool} = require("pg"); 
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false } 
+  });
+module.exports = pool;
+
 
